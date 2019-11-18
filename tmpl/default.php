9aa74c60f8228ad521b155e4475b817f4e766d76
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-11-18 16:02:19 +0000

    Added layout option and tidy template.

--- tmpl/default.php
+++ tmpl/default.php
@@ -19,20 +19,41 @@ $n_images = count ($images);
 if ($n_images == 0) {
     return;
 }
+
+$wrapper = 'div';
+$has_details = false;
+if (!empty($images->images0->caption) || !empty($images->images0->credit)) {
+    $has_details = true;
+    $wrapper = 'figure';
+}
+
 ?>
-<div>
-    <?php if ($module->showtitle): ?>
-    <<?php echo $hx; ?>><?php echo $module->title; ?></<?php echo $hx; ?>>
-    <?php endif; ?>
-    <?php if($n_images > 1) : ?>
-    <!-- @TOTO -->
-    <?php else: /* @TODO - need to think about credit lines. */?>
+<?php if ($module->showtitle): ?>
+<<?php echo $hx; ?>><?php echo $module->title; ?></<?php echo $hx; ?>>
+<?php endif; ?>
+<?php if($n_images > 1) : ?>
+<!-- @TOTO -->
+<?php else: /* @TODO - need to think about credit lines. */?>
+<<?php echo $wrapper; ?> class="u-image-cover  js-image-cover" style="postiion: relative;">
     <img src="<?php echo JURI::base() . $images->images0->image; ?>" width="600" alt="<?php echo $images->images0->alt; ?>">
+    <?php if ($has_details): ?>
+    <figcaption style="
+        position: absolute;
+        top: 0;
+        background: rgba(0,0,0,0.5);
+        color: #fff;
+        padding: 0.5em;
+    ">
+        <details>
+            <summary>Info</summary>
+            <?php if (!empty($images->images0->caption)) : ?>
+            <?php echo Markdown::defaultTransform($images->images0->caption); ?>
+            <?php endif; ?>
+            <?php if (!empty($images->images0->credit)) : ?>
+            <?php echo Markdown::defaultTransform($images->images0->credit); ?>
+            <?php endif; ?>
+        </details>
+    </figcaption>
     <?php endif; ?>
-    <?php if (!empty($images->images0->caption)) : ?>
-    <?php echo Markdown::defaultTransform($images->images0->caption); ?>
-    <?php endif; ?>
-    <?php if (!empty($images->images0->credit)) : ?>
-    <?php echo Markdown::defaultTransform($images->images0->credit); ?>
-    <?php endif; ?>
-</div>
+</<?php echo $wrapper; ?>>
+<?php endif; ?>
