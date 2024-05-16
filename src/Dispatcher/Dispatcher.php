71145ec686df8ea684ea71e3900c2ebd756f70ae
Andy Kirk <andy@andykirk.net>
2024-05-16 16:41:34 +0100

    J4

---
+++ src/Dispatcher/Dispatcher.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace NPEU\Module\Image\Site\Dispatcher;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+use Joomla\CMS\Helper\ModuleHelper;
+
+defined('_JEXEC') or die;
+
+/**
+ * Dispatcher class for mod_image
+ *
+ * @since  4.4.0
+ */
+class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    /**
+     * Returns the layout data.
+     *
+     * @return  array
+     */
+    protected function getLayoutData(): array
+    {
+        $data   = parent::getLayoutData();
+        $params = $data['params'];
+
+        /*
+        This module doesn't require any extra data but leaving this here in case that changes.
+        $data['stuff'] = $this->getHelperFactory()
+            ->getHelper('ImageHelper')
+            ->getStuff($params, $this->getApplication());
+        */
+        return $data;
+    }
+}
